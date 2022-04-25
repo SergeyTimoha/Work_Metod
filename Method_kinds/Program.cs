8f7a8f57aa77276dc9d8e5abc879edc3f839f35a
Sergey <serega79ser@mail.ru>
2022-04-25 21:56:06 +0600

    4 основных вид метода

---
+++ Method_kinds/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+//Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор....");
+}
+Method1();
+
+//Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения !");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+    
+}
+//Method21(msg:"Текст!", count: 5);
+Method21(count: 2,msg:"Текст!");
+
+//Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while ( i < count)
+    {
+        result += text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(5,"work");
+Console.WriteLine(res);
