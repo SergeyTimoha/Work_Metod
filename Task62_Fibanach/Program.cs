0b375e56dbc22785a88f4f5a8ab0e5df5c1bfc2e
Sergey <serega79ser@mail.ru>
2022-04-29 23:45:59 +0600

    Нахождения фибоначи

---
+++ Task62_Fibanach/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int F1 = int.Parse(Console.ReadLine());
+int F2 = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+GetFibanacci(F1, F2, N);
+
+void GetFibanacci(int F1, int F2, int N)
+{
+    if (N >= 1)
+        Console.Write(F1 + " ");
+    if (N >= 2)
+        Console.Write(F2 + " ");
+    for (int i = 0; i < N - 2; i++)
+    {
+        F2 += F1;
+        F1 = F2 - F1;
+        Console.Write(F2 + " ");
+    }
+
+}
+Console.WriteLine();
