9ffc0a8f6892aae28ba7c84b4c7ea03b92dc38d7
Sergey <serega79ser@mail.ru>
2022-06-22 19:42:42 +0600

    Вывод массива

---
+++ Test_array/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int elementCount = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[elementCount];
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] =  new Random().Next(1,100);
+}
+Console.WriteLine("Вывод массива");
+Console.WriteLine(String.Join("  " , Array));
+Console.WriteLine($"Максимальное число массива:  {Array.Max()}");
+Console.WriteLine($"Максимальное число массива:  {Array.Min()}");
